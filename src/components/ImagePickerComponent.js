61d90455bbf9d4a62af39f7e828d4f065244c994
Noorbegaum K Hussain <noorbegaumkhussain@Noor-iMac.local>
2022-12-22 14:33:21 +0530

    made changes in image picker component

--- src/components/ImagePickerComponent.js
+++ src/components/ImagePickerComponent.js
@@ -1,23 +1,95 @@
-import {View, StyleSheet, Image} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  useWindowDimensions,
+  Alert,
+  Image,
+} from 'react-native';
+import React, {useState} from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import ImagePicker from 'react-native-image-crop-picker';
+const ImagePickerComponent = () => {
+  const {width, height} = useWindowDimensions();
+  const [imageUri, setImageUri] = useState('');
 
-const ImagePickerComponent = ({imageUri}) => {
+  const getImageFromCamera = async () => {
+    ImagePicker.openCamera({
+      width: 104,
+      height: 104,
+      cropping: true,
+    }).then(image => {
+      // console.log(image);
+      setImageUri(`file://${image.path}`);
+      //   const {path, filename, mime} = image;
+      //   getImageUri({path, filename, mime});
+    });
+  };
+
+  const getImageFromGallary = async () => {
+    ImagePicker.openPicker({
+      width: 104,
+      height: 104,
+      cropping: true,
+      includeBase64: true,
+    }).then(image => {
+      // console.log(image);
+      setImageUri(`file://${image.path}`);
+      const {path, filename, mime} = image;
+      //   getImageUri({path, filename, mime});
+    });
+  };
+
+  const createThreeButtonAlert = () =>
+    Alert.alert('Select Picture From', '', [
+      {
+        text: 'Camera',
+        onPress: () => getImageFromCamera(),
+      },
+      {
+        text: 'Cancel',
+        onPress: () => null,
+        style: 'cancel',
+      },
+      {
+        text: 'Gallary',
+        onPress: () => getImageFromGallary(),
+      },
+    ]);
   return (
-    // <View style={styles.imageContainer}>
-    <Image
-      source={
-        imageUri
-          ? {uri: imageUri}
-          : require('../../assets/images/restaurant.png')
-      }
-      style={styles.image}
-    />
-    // </View>
+    <View style={styles.imageContainer}>
+      <Image
+        source={
+          imageUri
+            ? {uri: imageUri}
+            : require('../../assets/images/restaurant.png')
+        }
+        style={styles.image}
+      />
+      <TouchableOpacity
+        onPress={() => {
+          createThreeButtonAlert();
+        }}>
+        <View style={styles.addImageContainer}>
+          <Icon name="camera-plus-outline" size={34} color="#8D8D8D" />
+        </View>
+      </TouchableOpacity>
+    </View>
   );
 };
 
 export default ImagePickerComponent;
 const styles = StyleSheet.create({
+  addImageContainer: {
+    height: 65,
+    width: 65,
+    borderRadius: 5,
+    marginRight: 24,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#CCCCCC',
+  },
   image: {
     height: 65,
     width: 65,
